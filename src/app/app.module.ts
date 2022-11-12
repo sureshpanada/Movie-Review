779fb9886677400e8e54f79b983af4cbadd7a6c0
Suresh Panada <sureshpanada@Sureshs-MacBook-Air.local>
2022-11-13 00:46:25 +0530

    commit for github pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MovieComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
